0801ef8da8792d4c98b932c98433ae17bc8adced
SauloEsteves14 <sauloesteves02@gmail.com>
2023-04-10 14:58:23 -0300

    refactor: alter calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,5 @@
-numero_1 = input('Digite o primeiro número: ')
-numero_2 = input('Digite o segundo número: ')
-operador = input('Digite a operação a ser realizada(+-/*):')
+while True:
+    numero_1 = input('Digite o primeiro número: ')
+    numero_2 = input('Digite o segundo número: ')
+    operador = input('Digite a operação a ser realizada(+-/*):')
+
